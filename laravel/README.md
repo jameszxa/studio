50a410f4081f3356da1b81b0f743c8226fa6c432
OtakuTV Official <otakutv015@gmail.com>
2025-04-15 06:24:20 +0000

    CREATE A DATABASE USING LARAVEL

---
+++ laravel/README.md
@@ -0,0 +1,12 @@
+# Laravel Backend for BuySmart
+
+This is a basic Laravel project to serve as the backend for the BuySmart Next.js application.
+
+## Setup
+
+1.  Install dependencies: `composer install`
+2.  Configure your database in `.env`
+3.  Run migrations: `php artisan migrate`
+4.  Start the server: `php artisan serve`
+
+Further instructions will be added here.
---
+++ laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+        return response()->json($product);
+    }
+}
